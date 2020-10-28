e726ef46723eea3c14ad43b278e7290bc67d92b1
JoanManuelRojo <joanmrojo@gmail.com>
2020-10-27 23:07:52 -0300

    Creación de menúes para el CRUD.

---
+++ src/proyecto_abmc/Proyecto_ABMC.java
@@ -0,0 +1,13 @@
+package proyecto_abmc;
+
+import Formularios.Frm_menu_principal;
+
+public class Proyecto_ABMC {
+
+    public static void main(String[] args) {
+
+        //Se indica que el Main va a ser el formulario de Menú Principal
+        Frm_menu_principal.main(args);
+    }
+
+}
